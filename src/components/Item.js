a67b88fbce86f3e8d76a1b3a8d000688d3e6b146
Winnie-Flora <contentcreatorwinnie@gmail.com>
2024-08-13 13:53:50 +0300

    Hooks State and Events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  function handleAddToCart() {
+    setInCart((inCart) => !inCart);
+  }
+
+  const itemClass = inCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddToCart}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
